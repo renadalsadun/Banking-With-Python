2d888a6aa6988fa239ce30b184fff888ace8742f
renad <xirnzsixteen@hotmail.com>
2025-03-09 20:49:23 +0300

    add a header to data (customers)

--- customer.py
+++ customer.py
@@ -1,13 +1,44 @@
 import csv 
 	
 
+header = [ 'account_id' , 'first_name' , 'last_name' , 'password' , 'balance_checking' , 'balance_savings' ] 
 
 
+content = []
 def reading_from_file(file_name):
-    # opening the CSV file 
-    with open(f'{file_name}.csv', mode ='r')as file: #The ‘with‘ keyword is used along with the open() method as it simplifies exception handling and automatically closes the CSV file.
-        return csv.reader(file, delimiter=';') 
+    with open(f'{file_name}.csv', mode='r') as file_reader:
+        content = csv.reader( file_reader , delimiter = ';' )
+        data = [dict(zip(header, row)) for row in content]
+        return data
+
+customers = reading_from_file('data')
 
 
-#     for lines in csvFile: print(lines)
-  
+
+print(type(customers))
+print(customers)
+
+class Customer():
+    def __init__(self):
+        pass
+
+
+
+
+# def reading_from_file(file_name):
+
+#     '''
+#     Create a new file, append header, the append the data readed from 'data.csv'
+#     '''
+
+#     open("bank_data.csv", "w")
+
+#     with open(f'bank_data.csv', mode ='a')as file_write: #The ‘with‘ keyword is used along with the open() method as it simplifies exception handling and automatically closes the CSV file.
+#         csvwriter = csv.writer(file_write, delimiter=';') 
+#         csvwriter.writerow((header))
+#         with open(f'{file_name}.csv', mode ='r')as file_read: #The ‘with‘ keyword is used along with the open() method as it simplifies exception handling and automatically closes the CSV file.
+#             content = csv.reader(file_read, delimiter=';') 
+#             for lines in content :
+#                 csvwriter.writerow((lines))
+         
+# reading_from_file('data')
