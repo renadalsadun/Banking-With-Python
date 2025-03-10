4619f72c952a2f927ccc1e9510a616029db12216
renad <xirnzsixteen@hotmail.com>
2025-03-10 03:20:33 +0300

    fix account id problem

--- customer.py
+++ customer.py
@@ -1,5 +1,6 @@
 import csv 
 	
+############################################### READING FILE
 
 header = [ 'account_id' , 'first_name' , 'last_name' , 'password' , 'balance_checking' , 'balance_savings' ] 
 
@@ -12,18 +13,17 @@ def reading_from_file(file_name):
     reads the data from file_name file, and compines it in a dictionary with the header
     '''
 
-
     with open(f'{file_name}.csv', mode='r') as file_reader:
         content = csv.reader( file_reader , delimiter = ';' )
-        data = [dict(zip(header, row)) for row in content]
+        data = [dict(zip(header, line)) for line in content]
         return data
 
 
+############################################### END OF READING FILE
 
-# # debugging
-# print(type(customers))
-# print(customers)
-# #end of debugging 
+
+
+###############################################  CUSTOMER CLASS
 
 class Customer():
 
@@ -33,63 +33,62 @@ class Customer():
     def __init__(self, first_name, last_name, password , balance_checking = None , balance_savings = None, account_id = None ):
         
         #if condition becausee existing customers - i.e. data- have id's but future customers do not 
-        if not account_id:
+        if account_id:
+            self.account_id = int(account_id)
+            
+        else:
             self.account_id = Customer.next_account_id
+            
         Customer.next_account_id += 1
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
-        self.balance_checking = balance_checking
-        self.balance_savings = balance_savings
+        self.balance_checking = float(balance_checking)
+        self.balance_savings = float(balance_savings)
 
 
 
-    def add_new_customer(self, first_name, last_name, password , balance_checking = None , balance_savings = None ):
+    def add_new_customer(first_name, last_name, password , balance_checking = None , balance_savings = None , account_id = None  ):
 
-        new_customer = Customer(first_name, last_name, password, balance_checking, balance_savings )
+        new_customer = Customer(first_name, last_name, password, balance_checking, balance_savings , account_id )
 
         # add to all customers list 
         Customer.all_customers.append(new_customer) 
 
+    def find_costumer( self , account_id ):
+        for customer in Customer.all_customers:
+            if customer.account_id == account_id:
+                return customer
+            
+        return None # if the customer id is not valid, return none ;;
+
 
     def __str__ (self):
-        return f'Account ID: {self.account_id}, Name {self.first_name} {self.last_name}'
-        
+        return f'Account ID: {self.account_id}, Name: {self.first_name} {self.last_name}'
 
 # adding the customers in data.csv file to the Customer class!!    
 existing_customers = reading_from_file('data')
 for existing_customer in existing_customers:
     Customer.add_new_customer(
-        existing_customer['account_id'],
         existing_customer['first_name'],
         existing_customer['last_name'],
         existing_customer['password'],
         existing_customer['balance_checking'],
-        existing_customer['balance_savings']
+        existing_customer['balance_savings'],
+        existing_customer['account_id']
+
     )
 
+
+############################################### END OF CUSTOMER CLASS
+
+#debugging 
 for customer in Customer.all_customers:
 
     print(customer)
 
+print(Customer.next_account_id)
+# Customer.add_new_customer('renad', 'alsadun', )
+#end of debugging
 
 
-
-    
-# def reading_from_file(file_name):
-
-#     '''
-#     Create a new file, append header, the append the data readed from 'data.csv'
-#     '''
-
-#     open("bank_data.csv", "w")
-
-#     with open(f'bank_data.csv', mode ='a')as file_write: #The ‘with‘ keyword is used along with the open() method as it simplifies exception handling and automatically closes the CSV file.
-#         csvwriter = csv.writer(file_write, delimiter=';') 
-#         csvwriter.writerow((header))
-#         with open(f'{file_name}.csv', mode ='r')as file_read: #The ‘with‘ keyword is used along with the open() method as it simplifies exception handling and automatically closes the CSV file.
-#             content = csv.reader(file_read, delimiter=';') 
-#             for lines in content :
-#                 csvwriter.writerow((lines))
-         
-# reading_from_file('data')
