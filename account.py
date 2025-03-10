79f9bd3ccb3f01460f0cc151494a818cee28e745
renad <xirnzsixteen@hotmail.com>
2025-03-11 01:24:04 +0300

    add Account class structure/methods

--- account.py
+++ account.py
@@ -3,11 +3,14 @@ class Account():
     def __init__(self):
         pass
 
-    def withdraw_from_savings():
+    def withdraw_from_savings(self, amount):
         pass
 
-    def withdraw_from_checking():
+    def withdraw_from_checking(self, amount):
         pass
 
-     
+    def transfer(self, amount, target_account_id, target_account):
+        pass
     
+    def check_balance():
+        pass
