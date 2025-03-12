ba86a4c99bc4b2e9f0132867703bb80b775ae479
renad <xirnzsixteen@hotmail.com>
2025-03-12 21:29:29 +0300

    fix header issue in write to file and log transaction

--- access_file.py
+++ access_file.py
@@ -29,6 +29,7 @@ def reading_from_file(file_name):
         print(f"Error: Could not read the CSV file '{file_name}.csv")
         return [dict.fromkeys(header, '')]
 
+
     
 def write_to_file(file_name, customer):
     """Appends a new customer's data to the CSV file without removing previous data"""
@@ -36,12 +37,19 @@ def write_to_file(file_name, customer):
         # to check if file exsits or not
         file = os.path.isfile(f"{file_name}.csv") #returns true if file exists, false if it doesnt
         
+        if file and os.stat(f"{file_name}.csv").st_size == 0:  # file exists BUT is empty
+            write_header = True
+        else:
+            write_header = False
 
         with open(f"{file_name}.csv", mode="a", newline="") as file_writer:
             writer = csv.writer(file_writer, delimiter=";")
 
-            if not file: #if it doesnt exists
+            
+            if not file or write_header:  #if it doesnt exists , or it exists but empty
                 writer.writerow(header)
+
+
 
             writer.writerow([
                 customer.account_id,
@@ -54,21 +62,30 @@ def write_to_file(file_name, customer):
     except Exception as e:
         print(f"Error writing to file: {e}")
 
+
+
 def log_transaction(account_id, transaction_type, amount, balance):
     """ Logs a transaction in the transactions.csv file """
     try:
         # to check if file exsits or not
         file = os.path.isfile("transactions.csv") #returns true if file exists, false if it doesnt
+        if file and os.stat("transactions.csv").st_size == 0:  # file exists BUT is empty
+            write_header = True
+        else:
+            write_header = False
 
         with open("transactions.csv", mode="a", newline="") as file_writer:
             writer = csv.writer(file_writer, delimiter=";")
-            if not file: #if it doesnt exists
+
+            if not file or write_header: #if it doesnt exists , or it exists but empty
                 writer.writerow(transaction_header)
 
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             writer.writerow([timestamp, account_id, transaction_type, amount, balance])
     except Exception as e:
         print(f"Error logging transaction: {e}")
+
+
 
 def update_file(file_name, row_index, col_index, new_value):
     with open(file_name, mode='r', newline='') as infile:
